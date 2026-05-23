31735ba17f7377a02459fd979172d108a10e77c6
Vazid <vazidnh3@gmail.com>
2026-05-23 15:28:09 +0530

    Initail commit for auth service microservice

---
+++ TradeStream_Auth_Service/src/test/java/com/tradestream/authservice/TradeStreamAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradestream.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeStreamAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
